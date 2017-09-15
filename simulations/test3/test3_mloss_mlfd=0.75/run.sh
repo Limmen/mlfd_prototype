d38d8207d7a9992380c8122d5f896f0d47608b58
Kim Hammar <kimham@kth.se>
2017-09-15 07:39:44 +0200

    add some simulations

---
+++ simulations/test3/test3_mloss_mlfd=0.75/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$SPARK_HOME/bin/spark-submit --class "kth.se.ii2202.mlfd_prototype.Main" mlfd_prototype-assembly-0.1.0-SNAPSHOT.jar --test 1 --crash 0.001 --mloss 0.75 --sdev 100 --pmargin 3 --geof 50.0 --geoc 20 --bwf 1000 --bwc 30 --pattern --workers 100 --sdevc 30 --hbtimeout 2 --testdur 30 --samplew 200 --defaultmean 3000 --defaultsdev 1000 --learnrate 0.0000000001 --regp 0.3 --iter 10 --batchsize 100
